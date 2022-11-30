67b6ca6f8c1a80cce12d630b16ccca6d0bff4315
Gosia Brozek <gosikbrozek1997@gmail.com>
2022-11-30 17:34:16 +0100

    Style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import React from 'react'
 export const Header = () => {
   return (
     <div className='header'>
+        <img className='earthImg' src="/images/2108189.png" alt="earth" />
         <h3>my travel journal.</h3>
     </div>
   )
